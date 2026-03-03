c4f711037c15aa75e75a09126a6729b63afd2bcb
hanan-haider <137984110+hanan-haider@users.noreply.github.com>
2026-03-03 13:18:05 +0500

    .

---
+++ dfadetect/cnn_feature.py
@@ -0,0 +1,13 @@
+
+
+from dataclasses import dataclass, field
+from typing import List
+
+import torch
+import torchaudio
+
+
+@dataclass
+class CNNFeaturesSetting:
+    frontend_algorithm: List[str]= field(default_factory=lambda: ["mfcc"])
+    use_spectrogram: bool = True
--- train_models.py
+++ train_models.py
@@ -8,8 +8,9 @@ from typing import Callable, Dict, List, Optional, Union
 import torch
 import yaml
 
+
+from dfadetect.cnn_features import CNNFeaturesSetting
 #from dfadetect.agnostic_datasets.attack_agnostic_dataset import AttackAgnosticDataset
-#from dfadetect.cnn_features import CNNFeaturesSetting
 #from dfadetect.datasets import apply_feature_and_double_delta, lfcc, mfcc
 #from dfadetect.models import models
 #from dfadetect.models.gaussian_mixture_model import GMMDescent, flatten_dataset
