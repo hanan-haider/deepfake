a2d5df7f255c719cfe72a1a53caa7349b8d55961
Hanan Haider <137984110+hanan-haider@users.noreply.github.com>
2026-03-06 15:53:05 +0500

    .

--- dfadetect/cnn_features.py
+++ dfadetect/cnn_features.py
@@ -11,4 +11,35 @@ import torchaudio
 class CNNFeaturesSetting:
     frontend_algorithm: List[str]= field(default_factory=lambda: ["mfcc"])
     use_spectrogram: bool = True
+
+
+
+def prepare_feature_vector(
+    audio: torch.Tensor,
+    cnn_features_setting: CNNFeaturesSetting,
+    win_length: int = 400,
+    hop_length: int = 160,
+) -> torch.Tensor:
+
+    feature_vector = []
+
+    if "mfcc" in cnn_features_setting.frontend_algorithm:
+        mfcc_feature = MFCC_FN(audio)
+        feature_vector.append(mfcc_feature)
+
+    if "lfcc" in cnn_features_setting.frontend_algorithm:
+        lfcc_feature = LFCC_FN(audio)
+        feature_vector.append(lfcc_feature)
+
+    if cnn_features_setting.use_spectrogram:
+        stft_features = prepare_stft_features(audio, win_length, hop_length)
+        feature_vector += stft_features  # abs_mel, abs_angle
+
+    assert len(feature_vector) >= 1, "Feature vector must contain at least one feature!"
+
+    feature_vector = torch.stack(feature_vector, dim=1)
+
+    # [batch_size, feature_num, 80, frames], where feature_num in {1,2,3,4}
+    return feature_vector
+
     
